6f96f5487455fe5d7cde0c56ddb2a4cfa0d2f909
Mateusz-Nalezinski <133319733+Mateusz-Nalezinski@users.noreply.github.com>
2025-08-12 20:01:28 +0200

    Create version.js

---
+++ src/version.js
@@ -0,0 +1,7 @@
+// version.js — wersja aplikacji i opis zmian
+export const APP_VERSION = "v1.0.3";
+export const APP_CHANGELOG = `
+- Dodano generowanie raportów PDF
+- Poprawiono etykiety punktów
+- Ulepszono działanie filtra pikiet
+`;
